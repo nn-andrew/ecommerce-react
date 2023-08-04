8cadb4c4937bb2a0e98a496fc15407ad20a8a9dd
nn-andrew <nguyen.andrew.quang@gmail.com>
2023-08-04 02:21:07 -0700

    minor tweak

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import ItemPage from './pages/ItemPage';
-import ShoppingCartPage from './pages/ShoppingCartPage';
-import FavoritedItemsPage from './pages/FavoritedItemsPage';
+import { ItemsProvider } from './contexts/ItemsContext'
 import { FavoriteProvider } from './contexts/FavoriteContext';
 import { ShoppingCartProvider } from './contexts/ShoppingCartContext';
 import MainLayout from './layouts/MainLayout';
-import { ItemsProvider } from './contexts/ItemsContext'
+import HomePage from './pages/HomePage';
+import ItemPage from './pages/ItemPage';
 import HoodiesPage from './pages/HoodiesPage';
 import SweatersPage from './pages/SweatersPage';
+import ShoppingCartPage from './pages/ShoppingCartPage';
+import FavoritedItemsPage from './pages/FavoritedItemsPage';
 
 function App() {
   return (
@@ -36,14 +36,3 @@ function App() {
 }
 
 export default App;
-
-/*
-technologies/concepts used:
-
-react context
-hooks
-components
-
-<a href="https://www.vecteezy.com/free-photos">Free Stock photos by Vecteezy</a>
-<a href="https://www.flaticon.com/free-icons/bag" title="bag icons">Bag icons created by gungyoga04 - Flaticon</a>
-*/
